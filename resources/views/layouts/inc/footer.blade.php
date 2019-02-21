b35b9cb6951b59e7e0b9263413ab222fb03e25a6
samthom <thomas.sam64@gmail.com>
2019-02-21 15:00:33 +0530

    style updated

--- resources/views/layouts/inc/footer.blade.php
+++ resources/views/layouts/inc/footer.blade.php
@@ -2,11 +2,12 @@
 <div class="container-fluid " style="background-color: #212121;">
     <div class="row p-5">
         <div class="col-md-3 text-left border-light  " style="font-size: 12px;height:200px !important">
-          <a href="#" class="d-block p-2 text-light" style="font-size: 12px;"> Home</a>
-          <a href="#" class="d-block p-2 text-light" style="font-size: 12px;">Events</a>
-          <a href="#" class="d-block p-2 text-light" style="font-size: 12px;"> Contact Us</a>
-          <a href="#" class="d-block p-2 text-light" style="font-size: 12px;"> Register</a>
-          <a href="" class="d-block p-2 text-light" style="font-size: 12px;">Login</a>
+          <a href="/" class="d-block p-2 text-light" style="font-size: 12px;"> Home</a>
+          <a href="/events" class="d-block p-2 text-light" style="font-size: 12px;">Events</a>
+          <a href="/contact" class="d-block p-2 text-light" style="font-size: 12px;"> Contact Us</a>
+          <a href="/register" class="d-block p-2 text-light" style="font-size: 12px;"> Register</a>
+          <a href="/login" class="d-block p-2 text-light" style="font-size: 12px;">Login</a>
+          <a href="/rules" class="d-block p-2 text-light" style="font-size: 12px;">Rules</a>
         </div>
         <div class="col-md-6" style="height:200px !important">
           <div class="mapouter" >
